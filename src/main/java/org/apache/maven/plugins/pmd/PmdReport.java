119218c60db52015142e5276434832eea4e1879e
Andreas Dangel <adangel@apache.org>
2019-04-03 20:27:24 +0200

    [MPMD-280] maven-pmd-plugin does not support targetJdk 12
    Update javadoc for parameter targetJdk.

--- src/main/java/org/apache/maven/plugins/pmd/PmdReport.java
+++ src/main/java/org/apache/maven/plugins/pmd/PmdReport.java
@@ -92,7 +92,7 @@ public class PmdReport
     /**
      * The target JDK to analyze based on. Should match the source used in the compiler plugin. Valid values are
      * currently <code>1.3</code>, <code>1.4</code>, <code>1.5</code>, <code>1.6</code>, <code>1.7</code>,
-     * <code>1.8</code>, <code>9</code>, and <code>10</code>.
+     * <code>1.8</code>, <code>9</code>, <code>10</code>, <code>11</code>, and <code>12</code>.
      * <p>
      *   <b>Note:</b> this parameter is only used if the language parameter is set to <code>java</code>.
      * </p>
