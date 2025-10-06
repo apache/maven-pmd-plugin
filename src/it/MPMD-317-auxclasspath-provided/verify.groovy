63ea608ea79257c2f52422f0a0302062f93d433d
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-10-06 08:03:39 +0200

    fix tests

--- src/it/MPMD-317-auxclasspath-provided/verify.groovy
+++ src/it/MPMD-317-auxclasspath-provided/verify.groovy
@@ -36,6 +36,6 @@ assert 1 == auxclasspathLog.count( '[DEBUG] Using aggregated aux classpath:' )
 assert 2 == auxclasspathLog.count( 'module-a/target/classes' )
 
 // compile
-assert 2 == auxclasspathLog.count( 'org/apache/commons/commons-lang3/3.8.1/commons-lang3-3.8.1.jar' )
+assert 2 == auxclasspathLog.count( 'org/apache/commons/commons-lang3/3.18.0/commons-lang3-3.18.0.jar' )
 // provided
 assert 2 == auxclasspathLog.count( 'javax/servlet/javax.servlet-api/4.0.1/javax.servlet-api-4.0.1.jar' )
